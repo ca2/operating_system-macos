f0235ddaabf1240b95eb7d199d1bf3713c924eb1
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2021-08-09 03:49:52 -0300

    Delete thirdparty/include directory

--- thirdparty/include/mpg123_asm.h
+++
@@ -1,9 +0,0 @@
-//
-//  mpg123_asm.h
-//  os
-//
-//  Created by Camilo Sasuke Tsumanuma on 12/10/13.
-//  Copyright (c) 2013 ca2 Desenvolvimento de Sofware Ltda. All rights reserved.
-//
-
-#define ASMALIGN_BALIGN
